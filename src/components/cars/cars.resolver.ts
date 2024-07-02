11c19783ca57cd11954bc1c0038b0949fb89c0f5
BelugaWhaleSam <sameermohd2k1@gmail.com>
2024-07-02 16:27:26 +0530

    error-handling&&entities

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,5 +1,6 @@
 import { Query, Resolver } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
+import { Car } from "./entities/car";
 
 @Resolver()
 
@@ -8,8 +9,10 @@ export class CarsResolver {
         console.log('CarsResolver created');
     }
 
-    @Query(returns => String)
-    public async cars() {
-        return "Hello Welcome to Cars!";
+    @Query(returns => [Car])
+    public async cars(): Promise<Car[]> {
+        return await this.carsService.getAllCars().catch((e) => {
+            throw e;
+        })
     }
 }
--- src/components/cars/cars.service.ts
+++ src/components/cars/cars.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Car } from './entities/car';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 
 export class CarsService {
+    constructor(@InjectRepository(Car) private carRepository: Repository<Car>) {
 
+    }
+
+    public async getAllCars(): Promise<Car[]> {
+        return await this.carRepository.find({}).catch((e) => {
+            throw new InternalServerErrorException(e)
+        })
+    }
 }
