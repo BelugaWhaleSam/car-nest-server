7e4510517b14d508335a9f8bd16fef7b35d23e6c
BelugaWhaleSam <sameermohd2k1@gmail.com>
2024-07-03 15:54:37 +0530

    mutation-query-added

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
 import { Car } from "./entities/car";
+import { NewCarInput } from "./dto/new-car.input";
 
 @Resolver()
 
@@ -12,6 +13,13 @@ export class CarsResolver {
     @Query(returns => [Car])
     public async cars(): Promise<Car[]> {
         return await this.carsService.getAllCars().catch((e) => {
+            throw e;
+        })
+    }
+
+    @Mutation(returns => Car) 
+    public async addNewCar(@Args("newCarData") newCarData: NewCarInput): Promise<Car> {
+        return await this.carsService.addCar(newCarData).catch((e) => {
             throw e;
         })
     }
